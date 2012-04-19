ceaba7f5c675d3d569a7f3f9ef52531a5999ca2c
John Philip Dorado <dorado.johnphilip@gmail.com>
2012-04-19 14:03:55 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExamReviewer::Application.config.secret_token = '9f76734ebd9f6c4ef821354090c33e1de886af487e81519f6e493b91f2c953f76c35eaada00bb2cf7fd9cda125e686651ae1ee933f601e6e74bab58bcdd51669'
