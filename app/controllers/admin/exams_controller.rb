288599439a3349a5e94cd2082636701dbf7c9fca
John Philip Dorado <dorado.johnphilip@gmail.com>
2012-04-20 19:16:43 +0800

    create, delete Exam added to Admin functions

--- app/controllers/admin/exams_controller.rb
+++ app/controllers/admin/exams_controller.rb
@@ -1,2 +1,26 @@
-class ExamsController < ApplicationController
+class Admin::ExamsController < ApplicationController
+  before_filter :authenticate_admin!
+   
+  def index
+    @exam = current_admin.exams
+  end
+  
+  def new
+    @exam = current_admin.exams.new
+  end
+  
+  def create
+    new_exam = current_admin.exams.new(params[:exam])
+    if new_exam.save
+      redirect_to admin_exams_path
+    else
+      render 'new'
+    end
+  end
+  
+  def destroy
+    exam = Exam.find(params[:id])
+    exam.destroy
+    redirect_to admin_exams_path
+  end
 end
