c2bdcee3a0d970d99ecf41c54b8f724866357de4
John Philip Dorado <dorado.johnphilip@gmail.com>
2012-04-24 16:30:23 +0800

    Combined questions and choices view in Admin class

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -15,17 +15,23 @@ class Admin::QuestionsController < ApplicationController
   def new
     exam = Exam.find(params[:exam_id])
     @question = exam.questions.new
+    3.times { @question.choices.build }
   end
   
   def edit
     @exam = Exam.find(params[:exam_id])
     @question = @exam.questions.find(params[:id])
+    @choices = @question.choices.all
   end
   
   def create
     exam = Exam.find(params[:exam_id])
-    question = exam.questions.new(params[:question])
+    question = exam.questions.new(:question => params[:question][:question])
+    choices = params[:question][:choices_attributes]
     if question.save
+      choices.each do |choice|
+        question.choices.create(:choice => choice[1]['choice'])
+      end
       redirect_to admin_exam_question_path(exam.id, question.id)
     else
       render 'new'
@@ -35,7 +41,12 @@ class Admin::QuestionsController < ApplicationController
   def update
     exam = Exam.find(params[:exam_id])
     question = exam.questions.find(params[:id])
-    if question.update_attributes(params[:question])
+    choices = params[:question][:choices_attributes]
+    if question.update_attributes(:question => params[:question][:question])
+      choices.each do |choice| 
+        choice_to_edit = question.choices.find(choice[1]['id'])
+        choice_to_edit.update_attributes(:choice => choice[1]['choice'])
+      end
       redirect_to admin_exam_question_path(exam.id, question.id)
     else
       render 'edit'
@@ -46,10 +57,7 @@ class Admin::QuestionsController < ApplicationController
     exam = Exam.find(params[:exam_id])
     question = exam.questions.find(params[:id])
     if question.destroy
-      redirect_to admin_exam_questions_path(exam.id)
-    else
-      render 'edit'
     end
+    redirect_to admin_exam_questions_path(exam.id) 
   end
-  
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,8 @@
 class Question < ActiveRecord::Base
   attr_accessible :question
   
-  has_many :choices
+  has_many :choices, :dependent => :destroy
   belongs_to :exam
+  
+  accepts_nested_attributes_for :choices, :reject_if => lambda { |a| a[:choice].blank? }
 end
