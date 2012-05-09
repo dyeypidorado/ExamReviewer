b09ead7a7b75adf8d1394108bc6f856a7822137d
John Philip Dorado <dorado.johnphilip@gmail.com>
2012-05-09 11:22:34 +0800

    "Coolblue" theme added

--- app/assets/javascripts/reviewers_jquery.js
+++ app/assets/javascripts/reviewers_jquery.js
@@ -1,4 +1,10 @@
-function next_item(link){
+function last_item(link, path){
+  check_answer(link, path);
+  $(".item").remove();
+}
+
+function next_item(link, path){
+  check_answer(link, path);
   $(link).parent(".item").hide();
   $(link).parent(".item").next(".item").toggle();
 }
@@ -11,16 +17,19 @@ function prev_item(link){
 function check_answer( link, path){
   var choice = $(link).parent(".item").find("input[type=radio]:checked").val();
   if (choice !=null) {
-    $.post(path, {choice: choice},
+    var reviewer = $(link).parent(".item").find("input[type=hidden]").first().val();
+    var question = $(link).parent(".item").find("input[type=hidden]").last().val();
+    $.post(path, {choice_id: choice, question_id: question, reviewer_id: reviewer },
     function(data){
       alert(data);
     });
-    $(link).parent(".item").children("input[type=hidden]").last().val("1");
-
-    $(link).parent(".item").find("input[type=radio]:not(:checked)").closest("label").hide();
+    
+    $(link).parent(".item").find("input[type=radio]:checked").replaceWith("You're answer was: ");
+    
     var unchosen = $(link).parent(".item").find("input[type=radio]:not(:checked)");
     for(var i = 0; i < unchosen.length; i++){
-      $(unchosen[i]).next("label").hide();
+      $(unchosen[i]).next("label").remove();
+      $(unchosen[i]).next("br").remove();
       $(unchosen[i]).hide();
     }
   }
