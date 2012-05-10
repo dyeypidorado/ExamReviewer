c7c8a1b588f21fcf45db008d38a8c501c35bf2db
Loudcloud <loudcloud@loudcloud-HP-Compaq-nx6310-EY322ES-ABV.(none)>
2012-05-10 14:05:48 +0800

    Merge branch 'master' of github.com:dyeypidorado/ExamReviewer
    Conflicts:
    	app/views/layouts/application.html.haml
    	app/views/static_pages/index.html.haml

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
     if admin_signed_in?
-      redirect_to rails_admin_path
+      redirect_to admin_admins_path
     elsif user_signed_in?
       redirect_to users_path
     end
